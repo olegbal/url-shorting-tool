2d3f2e1676cdf37b219dea9b5a513a25db8ffc65
olegbal <olegbalah@gmail>
2017-04-27 16:08:40 +0200

    add redirectToOriginalLink,showLinkInfo methods

---
+++ server/src/main/java/com/github/olegbal/urlshortingtool/controllers/ShortLinksController.java
@@ -0,0 +1,35 @@
+package com.github.olegbal.urlshortingtool.controllers;
+
+import com.github.olegbal.urlshortingtool.services.impl.LinkServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
+
+@RestController
+public class ShortLinksController {
+
+    @Autowired
+    private LinkServiceImpl linkService;
+
+    @RequestMapping("/api/v1/shortlinks/{linkValue}")
+    public RedirectView redirectToOriginalLink(@PathVariable String linkValue) {
+
+        String originalLink = linkService.checkLink(linkValue);
+
+        if (originalLink != null) {
+            return new RedirectView(originalLink, false);
+        }
+        return new RedirectView("/api/v1/");
+    }
+
+    @RequestMapping("/api/v1/shortlinks/{linkValue}/info")
+    public ResponseEntity showLinkInfo(@PathVariable String linkValue) {
+
+        return new ResponseEntity(linkService.getLinkInfoByShortLink(linkValue), HttpStatus.OK);
+    }
+
+}
