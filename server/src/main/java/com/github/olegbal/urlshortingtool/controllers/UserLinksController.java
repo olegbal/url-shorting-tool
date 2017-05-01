c83f5444ffe2daf449b054d08d82df4ba683fdcd
olegbal <olegbalah@gmail.com>
2017-05-01 20:18:46 +0200

    add UserLinkSController. Replace user-links method to UserLinksController

---
+++ server/src/main/java/com/github/olegbal/urlshortingtool/controllers/UserLinksController.java
@@ -0,0 +1,53 @@
+package com.github.olegbal.urlshortingtool.controllers;
+
+import com.github.olegbal.urlshortingtool.domain.dto.CreatedLinkResponseDto;
+import com.github.olegbal.urlshortingtool.domain.dto.LinkDto;
+import com.github.olegbal.urlshortingtool.services.impl.LinkServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping(path = "/api/v1/links")
+public class UserLinksController {
+
+
+    @Autowired
+    private LinkServiceImpl linkService;
+
+
+    @RequestMapping(path = "", params = "userId", method = RequestMethod.GET)
+    public ResponseEntity getUsersLinks(Pageable pageable, @RequestParam("userId") long id) {
+
+        return new ResponseEntity(linkService.findAllUsersLinks(pageable, id), HttpStatus.OK);
+    }
+
+    @PreAuthorize("@userPreAuthorizeService.checkRightsToUrlById(#request,#id)")
+    @RequestMapping(path = "", params = "userId", method = RequestMethod.POST)
+    public ResponseEntity createUserLink(HttpServletRequest request,
+                                         @RequestBody LinkDto linkDto, @RequestParam("userId") long id) {
+
+        CreatedLinkResponseDto responseDto = linkService.createLink(id, linkDto);
+        if (responseDto != null) {
+            return new ResponseEntity(responseDto, HttpStatus.OK);
+        }
+        return new ResponseEntity(HttpStatus.NOT_MODIFIED);
+
+    }
+
+    @PreAuthorize("@userPreAuthorizeService.checkRightsToUrlById(#request,#id)")
+    @RequestMapping(path = "", params = "userId", method = RequestMethod.PUT)
+    public ResponseEntity updateUserLink(HttpServletRequest request,
+                                         @RequestBody LinkDto linkDto, @RequestParam("userId") long id) {
+
+        if (linkService.updateLink(id, linkDto)) {
+            return new ResponseEntity(HttpStatus.OK);
+        }
+        return new ResponseEntity(HttpStatus.NOT_MODIFIED);
+    }
+}
