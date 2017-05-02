6bf7cdf2e6545f8f9315a293eb75c21c05e5359c
olegbal <olegbalah@gmail.com>
2017-05-02 04:29:54 +0200

    add link-exists modal component

---
+++ client/src/app/components/modals/link-exists.modal.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { Modal } from 'ng2-modal-dialog/modal.module';
+
+@Component({
+  selector: 'link-exists-modal',
+  templateUrl: '../../templates/link-exists.modal.html',
+  styleUrls: ['../../styles/link-exists.modal.css']
+})
+// the Modal import allows the usage of the @Modal alias that adds the Modal functions.
+@Modal()
+export class LinkExistsModal {
+
+  closeModal: Function;
+
+  originalLink;
+
+  // Will fetch the userCreds passed from the callback.
+
+  constructor() { }
+
+  onCancel(): void {
+    this.closeModal();
+  }
+}
