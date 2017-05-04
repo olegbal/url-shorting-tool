447c6245ede84ce52db07bd4cacf2aa72a803308
olegbal <olegbalah@gmail.com>
2017-05-04 03:02:17 +0200

    add case sensitive query

--- server/src/main/java/com/github/olegbal/urlshortingtool/respositories/UserRepository.java
+++ server/src/main/java/com/github/olegbal/urlshortingtool/respositories/UserRepository.java
@@ -4,12 +4,14 @@ import com.github.olegbal.urlshortingtool.domain.entity.Role;
 import com.github.olegbal.urlshortingtool.domain.entity.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.Set;
 
 public interface UserRepository extends CrudRepository<User, Long> {
 
+    @Query(value = "SELECT * from users WHERE BINARY login = ?1", nativeQuery = true)
     User findByLogin(String login);
 
 }
