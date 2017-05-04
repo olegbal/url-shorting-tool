c7d3cc26122b5556212c08a7c1336065e9a9bc9e
olegbal <olegbalah@gmail.com>
2017-05-04 03:03:39 +0200

    add application-test.properties for testing

--- server/src/main/java/com/github/olegbal/urlshortingtool/Application.java
+++ server/src/main/java/com/github/olegbal/urlshortingtool/Application.java
@@ -2,7 +2,9 @@ package com.github.olegbal.urlshortingtool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
 
 @SpringBootApplication
 @EnableScheduling
