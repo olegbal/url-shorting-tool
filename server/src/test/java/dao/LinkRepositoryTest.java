30bc179a85f41de96e0f27882d5d06ac7d4357e5
olegbal <olegbalah@gmail.com>
2017-05-04 03:21:50 +0200

    optimize imports

--- server/src/test/java/dao/LinkRepositoryTest.java
+++ server/src/test/java/dao/LinkRepositoryTest.java
@@ -11,9 +11,7 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
