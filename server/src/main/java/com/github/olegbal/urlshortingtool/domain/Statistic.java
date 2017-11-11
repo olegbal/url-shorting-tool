d682253566b7ecdea58057cbc4c47f93da7cc0ac
olegbal <olegbalah@gmail.com>
2017-11-12 02:10:05 +0300

    Extend Statistic entity

--- server/src/main/java/com/github/olegbal/urlshortingtool/domain/Statistic.java
+++ server/src/main/java/com/github/olegbal/urlshortingtool/domain/Statistic.java
@@ -14,17 +14,23 @@ public class Statistic {
     @Column(name="deleted_links_count")
     private long deletedLinksCount;
 
+    @Column(name="created_links_count")
+    private long createdLinksCount;
 
+    @Column(name="registered_users_count")
+    private long registeredUsersCount;
 
-//    @Column(name="total_clicks_count")
-//    private long totalClicksCount;
-//
-//    @Column(name="registered_users_count")
-//    private long registeredUsersCount;
-//
-//    private long registeredAdministrators;
-//
-//    private long usersCount;
+    @Column(name="user_accounts_amount")
+    private long userAccountsAmount;
+
+    @Column(name="administrator_accounts_amount")
+    private long administratorAccountsAmount;
+
+    @Column(name="total_clicks_count")
+    private long totalClicksCount;
+
+    @Column(name="top_user_id")
+    private long topUserId;
 
     public long getStatisticId() {
         return statisticId;
@@ -40,5 +46,53 @@ public class Statistic {
 
     public void setDeletedLinksCount(long deletedLinksCount) {
         this.deletedLinksCount = deletedLinksCount;
+    }
+
+    public long getCreatedLinksCount() {
+        return createdLinksCount;
+    }
+
+    public void setCreatedLinksCount(long createdLinksCount) {
+        this.createdLinksCount = createdLinksCount;
+    }
+
+    public long getRegisteredUsersCount() {
+        return registeredUsersCount;
+    }
+
+    public void setRegisteredUsersCount(long registeredUsersCount) {
+        this.registeredUsersCount = registeredUsersCount;
+    }
+
+    public long getUserAccountsAmount() {
+        return userAccountsAmount;
+    }
+
+    public void setUserAccountsAmount(long userAccountsAmount) {
+        this.userAccountsAmount = userAccountsAmount;
+    }
+
+    public long getAdministratorAccountsAmount() {
+        return administratorAccountsAmount;
+    }
+
+    public void setAdministratorAccountsAmount(long administratorAccountsAmount) {
+        this.administratorAccountsAmount = administratorAccountsAmount;
+    }
+
+    public long getTotalClicksCount() {
+        return totalClicksCount;
+    }
+
+    public void setTotalClicksCount(long totalClicksCount) {
+        this.totalClicksCount = totalClicksCount;
+    }
+
+    public long getTopUserId() {
+        return topUserId;
+    }
+
+    public void setTopUserId(long topUserId) {
+        this.topUserId = topUserId;
     }
 }
